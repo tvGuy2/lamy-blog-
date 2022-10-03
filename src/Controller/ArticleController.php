9e68768fdd13c8e83c6f4122c7a9f9dfad4205f0
Tanguy Vallot <tanguyvallot39@gmail.com>
2022-10-03 11:47:05 +0200

    add pagination articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleType;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentaireRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ArticleController extends AbstractController
 {
     private ArticleRepository $aricleRepository;
+    private CommentaireRepository $commentaireRepository;
 
     //Demander à symfony d'injecter une instance de ArticleRepository
     // à la création du controlleur (instance de ArticleControlleur
-    public function __construct(ArticleRepository $aricleRepository)
+    public function __construct(ArticleRepository $aricleRepository , CommentaireRepository $commentaireRepository)
     {
         $this->aricleRepository = $aricleRepository;
+        $this->commentaireRepository=$commentaireRepository;
     }
 
 
@@ -24,14 +34,24 @@ class ArticleController extends AbstractController
     // A l'appel de la méthode symfony va créer un objet de la classe ArticleRepossitory
     // et le passer en paramètre de la méthode
     // Mécanisme : INJECTION DE DEPENDANCES
-    public function getArticles(): Response
+    public function getArticles(PaginatorInterface $paginator , Request $request): Response
     {
 
         // Récupérer les infos dans la base de données
         // Le controlleur fait appel au Modèle(une classe du modèle)
         // Afin de récupérer la liste des articles
         // $repository = new ArticleRepository();
-        $articles = $this->aricleRepository->findBy([],["createdAt" => "DESC",]);
+
+        //Mise en place de la pagination
+
+
+        $articles = $paginator->paginate(
+            $this->aricleRepository->findBy([],["createdAt" => "DESC",]), /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+
 
 
         return $this->render('article/index.html.twig',[
@@ -51,11 +71,49 @@ class ArticleController extends AbstractController
         // Le controlleur fait appel au Modèle(une classe du modèle)
         // Afin de récupérer la liste des articles
         // $repository = new ArticleRepository();
-        $article = $this->aricleRepository->findOneBy(["slug" => $slug] );
 
+
+
+        $article = $this->aricleRepository->findOneBy(["slug" => $slug] );
+        $commentaires = $this->commentaireRepository->findBy(["article_id" => $this->aricleRepository->findBy(["slug => $slug"])]);
 
         return $this->render('article/article.html.twig',[
-            "article" => $article
+            "article" => $article,
+            "commentaires" => $commentaires
         ]);
+    }
+
+
+
+    #[Route('/articles/nouveau', name: 'app_articles_nouveau',priority: 1)]
+
+    // A l'appel de la méthode symfony va créer un objet de la classe ArticleRepossitory
+        // et le passer en paramètre de la méthode
+        // Mécanisme : INJECTION DE DEPENDANCES
+    public function insert(SluggerInterface $slugger): Response
+    {
+        $article = new Article();
+        // Création du formulaire
+        $formArticle = $this->createForm(ArticleType::class,$article);
+        // Appel de le vue twig permettant d'afficher le formulaire
+
+        return $this->renderForm('article/nouveau.html.twig',[
+            'formArticle' => $formArticle
+            ]
+        );
+
+
+
+
+        /*$article->setTitre('Nouvel article 2')
+            ->setContenu("Contenu du nouvel article 2")
+            ->setSlug($slugger->slug($article->getTitre())->lower())
+            ->setCreatedAt(new \DateTime());
+
+        // Propre à symfony 6
+        $this->aricleRepository->add($article,true);
+
+        return $this->redirectToRoute("app_articles");
+        */
     }
 }
